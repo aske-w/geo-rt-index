f514f683b6387b7906eba92dab69879ff5797ab8
Aske Wachs <aske@wachs.dk>
2024-02-23 14:17:05 +0000

    helpers

---
+++ include/optix_helpers.cuh
@@ -0,0 +1,16 @@
+#ifndef OPTIX_HELPERS_CUH
+#define OPTIX_HELPERS_CUH
+
+
+#ifdef DEBUG
+#define OPTIX_CHECK( call )                                             \
+  {                                                                     \
+    OptixResult res = call;                                             \
+    if( res != OPTIX_SUCCESS )                                          \
+      {                                                                 \
+        fprintf( stderr, "Optix call (%s) failed with code %d (line %d)\n", #call, res, __LINE__ ); \
+        exit( 2 );                                                      \
+      }                                                                 \
+  }
+#endif
+#endif // OPTIX_HELPERS_CUH
