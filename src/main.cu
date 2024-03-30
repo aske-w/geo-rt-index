e88fc30b0634f7d2dbbad2e671a2ea25e2a6f271
Aske Wachs <aske@wachs.dk>
2024-03-30 17:23:55 +0000

    63m points

--- src/main.cu
+++ src/main.cu
@@ -78,10 +78,11 @@ int main() {
 
 
     cuda_buffer /*curve_points_d,*/ as;
-	const uint32_t num_points = (1 << 29) + (1 << 28) + (1 << 26); // = 872,415,232 = 7.76 GB worth of points
-	const uint32_t num_in_range = 1 << 1;
-	const auto query = Aabb{455, 333, 1000, 444};
-	const auto space = Aabb{0, 0, 20000, 2000};
+//	const uint32_t num_points = (1 << 29) + (1 << 28) + (1 << 26); // = 872,415,232 = 7.76 GB worth of points
+	const uint32_t num_points = (1 << 25) + (3 * 1 << 23) + (1 << 22); // = 62,914,560
+	const uint32_t num_in_range = 1 << 23;
+	const auto query = Aabb{140, 50, 180, 60};
+	const auto space = Aabb{-180, -90, 180, 90};
 	const bool shuffle = !DEBUG;
 	std::vector<Point> points;
 	MEASURE_TIME("Generating points",
