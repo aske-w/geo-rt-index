422bab03fb963ef2ee530d7c95fbf92b2b6ef374
Aske Wachs <aske@wachs.dk>
2024-04-08 11:14:54 +0000

    Run tested kernel on GPU also

--- tests/helpers/spatial_helpers/contains.cu
+++ tests/helpers/spatial_helpers/contains.cu
@@ -8,16 +8,52 @@
 #include "helpers/spatial_helpers.cuh"
 #include "../../generators/aabb.cuh"
 #include "../../generators/point.cuh"
+#include <vector>
 
 using namespace geo_rt_index::helpers;
 using std::cout, std::to_string;
 
-static inline auto FloatMinMaxRand()
+enum class Location : uint8_t
 {
-	return Catch::Generators::random(std::numeric_limits<float>::lowest(),std::numeric_limits<float>::max());
+	Host = 1,
+	Device = 2,
+	Both = Location::Host | Location::Device
+};
+
+constexpr inline bool operator&(const Location rhs, const Location lhs)
+{
+	return static_cast<uint8_t>(lhs) & static_cast<uint8_t>(rhs);
 }
 
-TEST_CASE("Points on AABB edge are not contained in AABB", "")
+template<Location location>
+void Inside();
+
+template<Location location>
+void Boundary();
+
+template<Location location>
+void Outside();
+
+__global__ static void ContainsWrapper(const Aabb& aabb, const Point& point, bool& result)
+{
+	result = SpatialHelpers::Contains(aabb, point);
+}
+
+TEST_CASE("Points on AABB edge are not contained in AABB CPU", "[CPU]")
+{
+	Boundary<Location::Host>();
+}
+TEST_CASE("Points on AABB edge are not contained in AABB GPU", "[GPU]")
+{
+	Boundary<Location::Device>();
+}
+TEST_CASE("Points on AABB edge are not contained in AABB Both", "")
+{
+	Boundary<Location::Both>();
+}
+
+template<Location location>
+void Boundary()
 {
 	const float float_max = GENERATE(
 	    1e10f,
@@ -36,10 +72,45 @@ TEST_CASE("Points on AABB edge are not contained in AABB", "")
 	    Point{float_max, float_min},
 	    Point{float_max, float_max}
  	);
-	REQUIRE_FALSE(SpatialHelpers::Contains(bbox, point));
+	bool result = false;
+	if (location & Location::Device)
+	{
+		bool temp_result = true;
+		cuda_buffer bbox_d, point_d, result_d;
+		bbox_d.alloc_and_upload<Aabb>({bbox});
+		point_d.alloc_and_upload<Point>({point});
+		result_d.alloc(sizeof(bool));
+		ContainsWrapper<<<1,1>>>(*bbox_d.ptr<const Aabb>(), *point_d.ptr<const Point>(), *result_d.ptr<bool>());
+		result_d.download(&temp_result, 1);
+		CHECK_FALSE(temp_result);
+		result |= temp_result;
+	}
+	if (location & Location::Host)
+	{
+		bool temp_result = true;
+		temp_result = SpatialHelpers::Contains(bbox, point);
+		CHECK_FALSE(temp_result);
+		result |= temp_result;
+	}
+
+	REQUIRE_FALSE(result);
 }
 
+TEST_CASE("Points outside AABB are not contained in AABB CPU", "")
+{
+	Outside<Location::Host>();
+}
+TEST_CASE("Points outside AABB are not contained in AABB GPU", "")
+{
+	Outside<Location::Device>();
+}
 TEST_CASE("Points outside AABB are not contained in AABB", "")
+{
+	Outside<Location::Both>();
+}
+
+template<Location location>
+void Outside()
 {
 	const float float_max = GENERATE(
 	    1e10f,
@@ -70,11 +141,49 @@ TEST_CASE("Points outside AABB are not contained in AABB", "")
 	    Point{float_max, float_max + x},
 	    Point{float_max + x, float_max + x}
 	);
-	REQUIRE_FALSE(SpatialHelpers::Contains(bbox, point));
+
+	bool result = false;
+	if (location & Location::Device)
+	{
+		bool temp_result = true;
+		cuda_buffer bbox_d, point_d, result_d;
+		bbox_d.alloc_and_upload<Aabb>({bbox});
+		point_d.alloc_and_upload<Point>({point});
+		result_d.alloc(sizeof(bool));
+		ContainsWrapper<<<1,1>>>(*bbox_d.ptr<const Aabb>(), *point_d.ptr<const Point>(), *result_d.ptr<bool>());
+		result_d.download(&temp_result, 1);
+		CHECK_FALSE(temp_result);
+		result |= temp_result;
+	}
+	if (location & Location::Host)
+	{
+		bool temp_result = true;
+		temp_result = SpatialHelpers::Contains(bbox, point);
+		CHECK_FALSE(temp_result);
+		result |= temp_result;
+	}
+
+	REQUIRE_FALSE(result);
 }
 
 
-TEST_CASE("Points inside AABB are contained in AABB", "")
+TEST_CASE("Points inside AABB are contained in AABB CPU", "[CPU]")
+{
+	Inside<Location::Host>();
+}
+
+TEST_CASE("Points inside AABB are contained in AABB GPU", "")
+{
+	Inside<Location::Device>();
+}
+
+TEST_CASE("Points inside AABB are contained in AABB Both", "")
+{
+	Inside<Location::Both>();
+}
+
+template<Location location>
+void Inside()
 {
 	const float float_max = GENERATE(
 	    1e10f,
@@ -91,5 +200,27 @@ TEST_CASE("Points inside AABB are contained in AABB", "")
 	const float x = GENERATE_REF(take(50, random(std::nextafterf(float_min, std::numeric_limits<float>::max()), std::nextafterf(float_max, std::numeric_limits<float>::lowest()))));
 	const float y = GENERATE_REF(take(50, random(std::nextafterf(float_min, std::numeric_limits<float>::max()), std::nextafterf(float_max, std::numeric_limits<float>::lowest()))));
 	Point point{x, y};
-	REQUIRE(SpatialHelpers::Contains(bbox, point));
+
+	bool result = true;
+	if (location & Location::Device)
+	{
+		bool temp_result = false;
+		cuda_buffer bbox_d, point_d, result_d;
+		bbox_d.alloc_and_upload<Aabb>({bbox});
+		point_d.alloc_and_upload<Point>({point});
+		result_d.alloc(sizeof(bool));
+		ContainsWrapper<<<1,1>>>(*bbox_d.ptr<const Aabb>(), *point_d.ptr<const Point>(), *result_d.ptr<bool>());
+		result_d.download(&temp_result, 1);
+		CHECK(temp_result);
+		result &= temp_result;
+	}
+	if (location & Location::Host)
+	{
+		bool temp_result = false;
+		temp_result = SpatialHelpers::Contains(bbox, point);
+		CHECK(temp_result);
+		result &= temp_result;
+	}
+
+	REQUIRE(result);
 }
