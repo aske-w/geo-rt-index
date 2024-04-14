901f4acc430755f5fe87e4f30f965c6a8bed9fe9
Aske Wachs <aske@wachs.dk>
2024-04-14 09:41:52 +0000

    revert, use ProcessPoolExecutor

--- main.py
+++ main.py
@@ -1,117 +1,43 @@
-from time import sleep
 import cuspatial
-import cudf
-import cupy
 import cuspatial.utils
 import cuspatial.utils.column_utils
-import geopandas as gp
-import pandas as pd
-import numpy as np
-import cuda
 import time
-import pyarrow.parquet
-import shapely
-from shapely.geometry import *
-from shapely import wkt, from_wkb
 import pyarrow.parquet as pq
-import pyarrow
+import multiprocessing as mp
 from osgeo import ogr
+from concurrent.futures import ProcessPoolExecutor # ThreadPoolExecutor locks GIL
 
-NUM = (1 << 25) + (3 * 1 << 23) + (1 << 22) # 62,914,560
-# NUM = (1 << 26) # 67,108,864 - see error in bottom
-# NUM = 10
-np.random.seed(0)
-cupy.random.seed(0)
+BATCH_SIZE = 1 << 21
 
-p = pq.read_table("data/duniform_p26_s369.parquet")
+pool = ProcessPoolExecutor(mp.cpu_count())
 
-def destruct(g):
-  return g[0].x, g[0].y
+data = pq.read_table("data/duniform_p26_s369.parquet")
+
+def work(batch):
+  geom_col = batch["geometry"]
+  result = []
+  for geom in geom_col:
+    g = ogr.CreateGeometryFromWkb(geom.as_py())
+    result.append(g.GetX())
+    result.append(g.GetY())
+  return result
 
 t = time.perf_counter()
+batches: list = data.to_batches(BATCH_SIZE) # 1m
 xy = []
-batches: list = p.to_batches(1 << 20)
-for row in p.column("geometry"):
-  geom = ogr.CreateGeometryFromWkb(row.as_py())
-  xy.append(geom.GetX())
-  xy.append(geom.GetY())
-  # geom_col = batch["geometry"]
-  # result = [None] * batch.num_rows
-  # for i, geom in enumerate(geom_col):
-  #   g = ogr.CreateGeometryFromWkb(geom.as_py())
-  #   xy.append(g.GetX())
-  #   xy.append(g.GetY())
-
-
-# for batch in batches:
-
+for result in pool.map(work, batches):
+  xy += result
 print(f"converting to xy took: {time.perf_counter() - t:.3f} ms")
-print(len(xy))
-exit()
 
 t = time.perf_counter()
-df = gp.read_parquet("data/duniform_p22_s1337.parquet", ["geometry"])["geometry"]
-print(df.size)
-print(f"generating {NUM:,} xy points took: {time.perf_counter() - t:.3f} ms")
-print("loaded parquet file")
+points = cuspatial.GeoSeries.from_points_xy(xy)
+print(f"Creating points from interleaved columns took: {time.perf_counter() - t:.3f} ms")
 
-# t = time.perf_counter()
-# xy = []
-# for g in df:
-#     xy.append(g.x)
-#     xy.append(g.y)
-# print(f"converting to xy took: {time.perf_counter() - t:.3f} ms")
-
-t = time.perf_counter()
-points = cuspatial.from_geopandas(df)
-print(f"converting to cuspatial took: {time.perf_counter() - t:.3f} ms")
-del df
-
-# t = time.perf_counter()
-# with open("data/duniform_p26_s1278.pickle", "wb") as f:
-#     pickle.dump(points, f)
-# print(f"pickling took: {time.perf_counter() - t:.3f} ms")
-exit()
-# def worker(slice):
-#     return cuspatial.GeoSeries(slice)
-
-# points_fut = pool.submit(worker, df[:(1<<25)])
-# points2_fut = pool.submit(worker, df[(1<<25):])
-# # points = cuspatial.from_geopandas(df[:(1<<25)])
-# # points2 = cuspatial.from_geopandas(df[(1<<25):])
-# points = points_fut.result()
-# points.append(points2_fut.result())
-del df
-print(f"generating {NUM:,} xy points took: {time.perf_counter() - t:.3f} ms")
-print("loaded geoseries")
-print(points.head())
-
-# t = time.perf_counter()
-# x_points = (cupy.random.random(NUM) - 0.5) * 360
-# y_points = (cupy.random.random(NUM) - 0.5) * 180
-# print(f"generating {NUM:,} xy points took: {time.perf_counter() - t:.3f} ms")
-# sleep(3)
-
-
-# t = time.perf_counter()
-# xy = cudf.DataFrame({"x": x_points, "y": y_points}).interleave_columns()
-# print(f"interleave_columns took: {time.perf_counter() - t:.3f} ms")
-# sleep(3)
-
-
-# t = time.perf_counter()
-# points = cuspatial.GeoSeries.from_points_xy(xy)
-# del xy, x_points, y_points
-# print(f"Creating points from interleaved columns took: {time.perf_counter() - t:.3f} ms")
-# print(points.head())
-# exit()
-
-input()
+input("Press enter to run cuspatial.points_in_spatial_window")
 hit_start = time.perf_counter()
 hit = cuspatial.points_in_spatial_window(points, 0, 1, 0, 1) # beware that it is minx, maxx, miny, maxy
 print("hit took ", time.perf_counter() - hit_start)
 print(hit.head()) # lmao
-input()
 
 # bboxes_dict = {
 #     "minx": [-20.],
