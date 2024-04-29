a2364e81d860388a7f31634f478580fc0e667db8
Aske Wachs <aske@wachs.dk>
2024-04-29 05:58:33 +0000

    vm setup script

---
+++ vm_setup.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+sudo apt update
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install -y -V ca-certificates lsb-release wget libc6 python3.10 python3.10-distutils python3.10-dev libproj-dev swig python-is-python3
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8  2
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 2
+echo 1 | sudo update-alternatives --config python
+echo 1 | sudo update-alternatives --config python3
+
+wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+sudo apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+sudo apt update
+sudo apt install -y libarrow-dev libparquet-dev
+
+curl https://bootstrap.pypa.io/get-pip.py -O
+python3.10 get-pip.py
+echo 'export PATH="/home/ucloud/.local/bin:$PATH"' >> ~/.profile
+echo 'export PYTHONPATH="/home/ucloud/.local/lib/python3/dist-packages:$PYTHONPATH"' >> ~/.profile
+source ~/.profile
+
+pip3.10 install cmake
+sudo apt install -y cmake-curses-gui
+pip3.10 install \
+    --extra-index-url=https://pypi.nvidia.com \
+    cudf-cu12==24.4.* cuspatial-cu12==24.4.* cuproj-cu12==24.4.*
+
+git clone https://github.com/OSGeo/gdal.git
+mkdir -p gdal/build
+cd gdal/build
+git checkout v3.8.5
+cmake .. -DBUILD_PYTHON_BINDINGS=ON \
+    -DCMAKE_INSTALL_PREFIX=~/.local \
+    -DBUILD_APPS=OFF \
+    -DBUILD_GMOCK=OFF \
+    -DBUILD_TESTING=OFF
+make -j40 install
+echo 'export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$HOME/.local/lib"' >> ~/.profile
+echo 'export LIBRARY_PATH="${LD_LIBRARY_PATH}:$HOME/.local/lib"' >> ~/.profile
+echo 'export INCLUDE_PATH="${LD_LIBRARY_PATH}:$HOME/.local/include"' >> ~/.profile
+source ~/.profile
+
+git clone https://github.com/aske-w/geo-rt-index.git
