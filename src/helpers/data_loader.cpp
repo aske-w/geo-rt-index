e6dd0d5df747fc138965ee28fe92ab235311206a
Aske Wachs <aske@wachs.dk>
2024-05-04 13:12:09 +0000

    Revert "read single files in parallel instead of each file in parallel"
    This reverts commit 5558887987baafd00c853550e8ca1e00f6022f6f.

--- src/helpers/data_loader.cpp
+++ src/helpers/data_loader.cpp
@@ -20,47 +20,9 @@ using std::vector;
 
 static std::counting_semaphore load_limiter{std::ptrdiff_t{std::thread::hardware_concurrency()}};
 
-//! Check if a field is set for a given feature index
-inline auto IsSet(const uint8_t* buffer_not_null, int i)
-{
-	return buffer_not_null == nullptr || (buffer_not_null[i/8] >> (i%8)) != 0;
-}
-
-vector<Point> Convert(std::unique_ptr<ArrowArray> array, float _modifier)
+static vector<Point> Work(const std::string& path, const float modifier) noexcept
 {
 	load_limiter.acquire();
-	const auto wkb_child = array->children[0];
-	assert(wkb_child->n_buffers == 3);
-	const uint8_t* wkb_field_not_null = static_cast<const uint8_t*>(wkb_child->buffers[0]);
-	const int32_t* wkb_offset = static_cast<const int32_t*>(wkb_child->buffers[1]);
-	const uint8_t* wkb_field = static_cast<const uint8_t*>(wkb_child->buffers[2]);
-
-	vector<Point> private_result;
-	private_result.reserve(array->length);
-	for( long long i = 0; i < array->length; i++ )
-	{
-		if( !IsSet(wkb_field_not_null, i) )
-		{
-			throw std::runtime_error("wkb field is null: " + std::to_string(i));
-		}
-		const void* wkb = wkb_field + wkb_offset[i];
-		const int32_t length = wkb_offset[i+1] - wkb_offset[i];
-		OGRPoint point;
-		size_t consumed_bytes;
-		point.importFromWkb(reinterpret_cast<const unsigned char*>(wkb), length, wkbVariantOldOgc, consumed_bytes);
-		const auto x = static_cast<float>(point.getX()) * _modifier;
-		const auto y = static_cast<float>(point.getY()) * _modifier;
-		private_result.emplace_back(x, y);
-	}
-	// Release memory taken by the batch
-	array->release(&*array);
-//	D_PRINT("releasing %p\n", &*array);
-	load_limiter.release();
-	return private_result;
-};
-
-static void ReadFile(vector<Point>& result, const std::string& path, const float modifier) noexcept
-{
 	GDALDataset* poDS = GDALDataset::Open(path.c_str());
 	if( poDS == nullptr )
 	{
@@ -103,27 +65,66 @@ static void ReadFile(vector<Point>& result, const std::string& path, const float
 	schema.release(&schema);
 
 	// Iterate over batches
-	vector<std::future<vector<Point>>> futures;
+
+
+
+
+	vector<Point> private_result;
 	while(true)
 	{
-		auto array = std::make_unique<ArrowArray>();
-		if( stream.get_next(&stream, &*array) != 0 ||
-		    array->release == nullptr )
+		ArrowArray array;
+		if( stream.get_next(&stream, &array) != 0 ||
+		    array.release == nullptr )
 		{
 			break;
 		}
-		auto handle = std::async(std::launch::async, Convert, std::move(array), modifier);
-		futures.push_back(std::move(handle));
+
+		const auto wkb_child = array.children[0];
+		assert(wkb_child->n_buffers == 3);
+		const uint8_t* wkb_field_not_null = static_cast<const uint8_t*>(wkb_child->buffers[0]);
+		const int32_t* wkb_offset = static_cast<const int32_t*>(wkb_child->buffers[1]);
+		const uint8_t* wkb_field = static_cast<const uint8_t*>(wkb_child->buffers[2]);
+
+		// Lambda to check if a field is set for a given feature index
+		const auto IsSet = [](const uint8_t* buffer_not_null, int i)
+		{
+			return buffer_not_null == nullptr || (buffer_not_null[i/8] >> (i%8)) != 0;
+		};
+
+		private_result.reserve(array.length);
+		for( long long i = 0; i < array.length; i++ )
+		{
+			if( !IsSet(wkb_field_not_null, i) )
+			{
+				throw std::runtime_error("wkb field is null: " + std::to_string(i));
+			}
+			//				{
+			const void* wkb = wkb_field + wkb_offset[i];
+			const int32_t length = wkb_offset[i+1] - wkb_offset[i];
+			OGRPoint point;
+			size_t consumed_bytes;
+			point.importFromWkb(reinterpret_cast<const unsigned char*>(wkb), length, wkbVariantOldOgc, consumed_bytes);
+			//					OGRGeometryFactory::createFromWkb(wkb, nullptr, reinterpret_cast<OGRGeometry**>(&point), length);
+			//					assert(geom != nullptr);
+			//					assert(geom->getGeometryType() == OGRwkbGeometryType::wkbPoint);
+			const auto x = static_cast<float>(point.getX()) * modifier;
+			const auto y = static_cast<float>(point.getY()) * modifier;
+			private_result.emplace_back(x, y);
+			//					delete point;
+			//				}
+			//				else
+			//				{
+			//					printf("wkb_field[%lld] = null\n", i);
+			//				}
+		}
+		// Release memory taken by the batch
+		array.release(&array);
 	}
-	for(auto&& handle : futures)
-	{
-		auto future_result = handle.get();
-		result.reserve(future_result.size());
-		result.insert(result.end(), future_result.begin(), future_result.end());
-	}
+	load_limiter.release();
 	// Release stream and dataset
 	stream.release(&stream);
 	delete poDS;
+	return private_result;
 }
 
 //! from https://gdal.org/tutorials/vector_api_tut.html#reading-from-ogr-using-the-arrow-c-stream-data-interface
@@ -131,9 +132,17 @@ vector<Point> DataLoader::Load(const vector<std::string>& files, const float mod
 {
 	OGRRegisterAll();
 	vector<Point> result;
+	vector<std::future<vector<Point>>> futures;
 	for(auto path : files)
 	{
-		ReadFile(result, path, modifier);
+		auto handle = std::async(std::launch::async, Work, path, modifier);
+		futures.push_back(std::move(handle));
+	}
+
+	for(auto&& handle : futures)
+	{
+		auto future_result = handle.get();
+		result.insert(result.end(), future_result.begin(), future_result.end());
 	}
 	D_PRINT("size: %zu\n", result.size());
 	OGRCleanupAll();
