e2ff08c5c7099e007734bd9a69a01e5bacaa1445
Aske Wachs <aske@wachs.dk>
2024-05-08 13:37:39 +0000

    Revert "limit result check threads to hw threads"
    This reverts commit afd6de0f48b602a1c76d6966b08aff7725a61580.

--- src/main.cu
+++ src/main.cu
@@ -162,8 +162,7 @@ void Run(const std::vector<Point>& points)
 	MEASURE_TIME("result_d.download", result_d.download(result, num_points * num_queries););
 
 #ifdef VERIFICATION_MODE
-	static std::counting_semaphore load_limiter{std::ptrdiff_t{std::thread::hardware_concurrency()}};
-	MEASURE_TIME("Result check",
+	 MEASURE_TIME("Result check",
 		auto query_checker = [&](int i) 
 		{
 			uint32_t hit_count = 0;
@@ -193,7 +192,6 @@ void Run(const std::vector<Point>& points)
 					throw std::runtime_error("Unknown error");
 				}
 			}
-			load_limiter.release();
 		    return hit_count;
 		};
 
@@ -203,7 +201,6 @@ void Run(const std::vector<Point>& points)
 	    D_PRINT("Checking device results\n");
 		for(uint32_t i = 0; i < num_queries; i++)
 		{
-			load_limiter.acquire();
 			auto query_check_handle = std::async(std::launch::async, query_checker, i);
 		    futures.push_back(std::move(query_check_handle));
 		}
@@ -213,7 +210,7 @@ void Run(const std::vector<Point>& points)
 		    hit_sum += future.get();
 	    }
 		D_PRINT("Hit count: %u\n", hit_sum);
-	);
+	 );
 
 #endif
 	delete[] result;
