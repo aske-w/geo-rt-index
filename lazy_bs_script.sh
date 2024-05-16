8eecb2bc011ffd97e149a8e940363dfce32d7656
Aske Wachs <aske@wachs.dk>
2024-05-16 06:05:02 +0000

    update lazy run script

--- lazy_bs_script.sh
+++ lazy_bs_script.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 echo "begin @ $(date)"
-bs="dataset_scaling ds_check_each query_scaling aabb_layering rays_per_thread"
+bs="point_sorting modifier dataset_scaling query_scaling aabb_layering rays_per_thread"
 for b in $bs;
 do
     echo "$b"
-    python3 runner.py -d uniform -b "$b" --file-stem-suffix "*_r-11" --lo "-1" --hi 1 -p geo-rt-index --dry-run
-    python3 runner.py -d uniform -b "$b" --file-stem-suffix "*_r01" --lo 0 --hi 1 -p geo-rt-index --dry-run
+    python3 runner.py -d uniform -b "$b" --lo "-1" --hi 1 -p geo-rt-index --dry-run
+    python3 runner.py -d uniform -b "$b" --lo 0 --hi 1 -p geo-rt-index --dry-run
+    python3 runner.py -d normal -b "$b" --lo "-1" --hi 1 -p geo-rt-index --dry-run
+    python3 runner.py -d normal -b "$b" --lo 0 --hi 1 -p geo-rt-index --dry-run
     echo ""
 done
 echo "end @ $(date)"
