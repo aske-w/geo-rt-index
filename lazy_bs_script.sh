9d3d555ca4b86efc767b806fd9390b88424f046c
Aske Wachs <aske@wachs.dk>
2024-05-21 17:05:45 +0200

    cuspatial

--- lazy_bs_script.sh
+++ lazy_bs_script.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
 
 echo "begin @ $(date)"
-# bs="point_sorting modifier dataset_scaling query_scaling aabb_layering rays_per_thread"
-bs="aabb_layering rays_per_thread aabb_z_value ray_length"
+redo_umoo_bs="rays_per_thread aabb_layering point_sorting modifier dataset_scaling query_scaling"
+for b in $redo_umoo_bs;
+do
+    echo "$b"
+    python3 runner.py -d uniform -b "$b" --lo "-1" --hi 1 -p geo-rt-index
+done
+
+bs="aabb_z_value"
+for b in $bs;
+do
+    echo "$b"
+    python3 runner.py -d uniform -b "$b" --lo "-1" --hi 1 -p geo-rt-index
+    python3 runner.py -d normal -b "$b" --lo "-1" --hi 1 -p geo-rt-index
+    python3 runner.py -d normal -b "$b" --lo 0 --hi 1 -p geo-rt-index
+    echo ""
+done
+bs="ray_length"
 for b in $bs;
 do
     echo "$b"
@@ -22,5 +37,6 @@ do
     python3 runner.py -d uniform -b "$b" --lo 0 --hi 1 -p geo-rt-index --kernel-only
     python3 runner.py -d normal -b "$b" --lo "-1" --hi 1 -p geo-rt-index --kernel-only
     python3 runner.py -d normal -b "$b" --lo 0 --hi 1 -p geo-rt-index --kernel-only
+    python3 runner.py -d uniform -b "$b" --lo "-1" --hi 1 -p cuspatial
     echo ""
 done
