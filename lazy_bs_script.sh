191ad6d86afbe98b4a8cf94c75dd3474ff371527
Aske Wachs <aske@wachs.dk>
2024-05-23 10:24:58 +0200

    run modifier benchmark

--- lazy_bs_script.sh
+++ lazy_bs_script.sh
@@ -1,23 +1,7 @@
 #! /bin/bash
 
 echo "begin @ $(date)"
-redo_umoo_bs="rays_per_thread aabb_layering point_sorting modifier dataset_scaling query_scaling"
-for b in $redo_umoo_bs;
-do
-    echo "$b"
-    python3 runner.py -d uniform -b "$b" --lo "-1" --hi 1 -p geo-rt-index
-done
-
-bs="aabb_z_value"
-for b in $bs;
-do
-    echo "$b"
-    python3 runner.py -d uniform -b "$b" --lo "-1" --hi 1 -p geo-rt-index
-    python3 runner.py -d normal -b "$b" --lo "-1" --hi 1 -p geo-rt-index
-    python3 runner.py -d normal -b "$b" --lo 0 --hi 1 -p geo-rt-index
-    echo ""
-done
-bs="ray_length"
+bs="modifier"
 for b in $bs;
 do
     echo "$b"
@@ -25,18 +9,5 @@ do
     python3 runner.py -d uniform -b "$b" --lo 0 --hi 1 -p geo-rt-index
     python3 runner.py -d normal -b "$b" --lo "-1" --hi 1 -p geo-rt-index
     python3 runner.py -d normal -b "$b" --lo 0 --hi 1 -p geo-rt-index
-    echo ""
-done
-echo "end @ $(date)"
-
-bs="dataset_scaling query_scaling"
-for b in $bs;
-do
-    echo "$b"
-    python3 runner.py -d uniform -b "$b" --lo "-1" --hi 1 -p geo-rt-index --kernel-only
-    python3 runner.py -d uniform -b "$b" --lo 0 --hi 1 -p geo-rt-index --kernel-only
-    python3 runner.py -d normal -b "$b" --lo "-1" --hi 1 -p geo-rt-index --kernel-only
-    python3 runner.py -d normal -b "$b" --lo 0 --hi 1 -p geo-rt-index --kernel-only
-    python3 runner.py -d uniform -b "$b" --lo "-1" --hi 1 -p cuspatial
     echo ""
 done
